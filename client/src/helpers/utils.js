061ef325dfbe92014141b2d300215411a411e5dc
manu-xo5 <mohit.matwaya@gmail.com>
2020-12-20 15:09:14 +0530

    feat: helper for doing formdata inline

--- client/src/helpers/utils.js
+++ client/src/helpers/utils.js
@@ -4,3 +4,9 @@
  * @returns pulls value from obj passed in curryed fn
  */
 export const pull = key => obj => obj[key];
+
+export const mult = (...files) =>
+  files.reduce((acc, filesArgs) => {
+    acc.append(...filesArgs);
+    return acc;
+  }, new FormData());
